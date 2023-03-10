0c5672bd4ef73dbc14c9db013b71fc69cfad3381
Surajpawar83 <pawarsk88@gmail.com>
2023-03-10 20:09:42 +0530

    first commit

--- src/components/FooterStyles.js
+++ src/components/FooterStyles.js
@@ -1,60 +1,61 @@
-import styled from 'styled-components';
-   
+import styled from "styled-components";
+
 export const Box = styled.div`
+  bottom: 0;
   padding: 5px 5px;
-  background: #a69939;
+  background: #343a40;
   height: auto;
   width: 100%;
-  margin-top:20px;
+  margin-top: 20px;
   @media (max-width: 1000px) {
     padding: 10px 10px;
   }
 `;
-   
+
 export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    max-width: 1000px;
-    margin: 0 auto;
-    /* background: red; */
-`
-   
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 1000px;
+  margin: 0 auto;
+  /* background: red; */
+`;
+
 export const Column = styled.div`
   display: flex;
   flex-direction: column;
-  text-align: left;
+  text-align: center;
   margin-left: 10px;
 `;
-   
+
 export const Row = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, 
-                         minmax(185px, 1fr));
+  justify-content: center;
+  margin: 5px;
+  display: flex;
+  grid-template-columns: repeat(auto-fill, minmax(185px, 1fr));
   grid-gap: 20px;
-   
+
   @media (max-width: 1000px) {
-    grid-template-columns: repeat(auto-fill, 
-                           minmax(200px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   }
 `;
-   
+
 export const FooterLink = styled.a`
   color: #fff;
   margin-bottom: 20px;
   font-size: 18px;
   text-decoration: none;
-   
+
   &:hover {
-      color: blue;
-      font-size: 20px;
-      transition: 200ms ease-in;
+    color: red;
+    font-size: 20px;
+    transition: 200ms ease-in;
   }
 `;
-   
+
 export const Heading = styled.p`
   font-size: 24px;
   color: #fff;
-  margin-bottom: 40px;
+  margin-bottom: 10px;
   font-weight: bold;
 `;
