fd7d71841c692cfe6653b33276ea1dc22d9421b5
Priyanka9801 <116148848+Priyanka9801@users.noreply.github.com>
2023-03-11 04:17:31 +0530

    Add files via upload

---
+++ productvalidation.js
@@ -0,0 +1,16 @@
+import "../Styles/Header.css";
+const productvalidation = (values) => {
+  let errors = {};
+  if (!values.pname) {
+    errors.pname = "Product Name is required";
+  }
+  if (!values.price) {
+    errors.price = "Price is required";
+  }
+  if (!values.pcat) {
+    errors.pcat = "Category is required";
+  }
+  return errors;
+};
+
+export default productvalidation;
